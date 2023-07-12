d6170481c9cdd294a783249a9aae3f5f73ee20e3
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-07-12 14:23:27 +0300

    Update path to work locally

--- Computer Vision/Choosing metrics/workspace/utils.py
+++ Computer Vision/Choosing metrics/workspace/utils.py
@@ -5,17 +5,19 @@ import numpy as np
 
 def check_results(ious):
     # check the results
-    solution = np.load('data/exercise1_check.npy')
-    assert (ious == solution).sum() == 40, 'The iou calculation is wrong!'
-    print('Congrats, the iou calculation is correct!')
+    solution = np.load(
+        "Computer Vision\Choosing metrics\workspace\data\exercise1_check.npy"
+    )
+    assert (ious == solution).sum() == 40, "The iou calculation is wrong!"
+    print("Congrats, the iou calculation is correct!")
 
 
 def get_data():
-    """ simple wrapper function to get data """
-    with open('data/ground_truth.json') as f:
+    """simple wrapper function to get data"""
+    with open("Computer Vision\Choosing metrics\workspace\data\ground_truth.json") as f:
         ground_truth = json.load(f)
-    
-    with open('data/predictions.json') as f:
+
+    with open("Computer Vision/Choosing metrics/workspace/data/predictions.json") as f:
         predictions = json.load(f)
 
     return ground_truth, predictions
